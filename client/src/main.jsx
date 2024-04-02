ad1500c5ce28142e22870d1111e638a819887cf1
Brady-hash <bradydavis@Bradys-MacBook-Pro.local>
2024-04-01 19:14:59 -0500

    added pages and some main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx'
 import './index.css'
+import ChatRoom from './pages/Chatroom.jsx';
+import MessageHub from './pages/MessageHub.jsx';
+import MyProfile from './pages/MyProfile.jsx';
+
+const router = createBrowserRouter({
+  routes: [
+    {
+      path: '/',
+      element: <APP />,
+      errorElement: <NotFound />,
+      children: [
+        {
+          index: true,
+          element: <MessageHub />
+        },
+        {
+          path: "/myprofile",
+          element: <MyProfile />
+        },
+        {
+          path: "chatroom/:id",
+          element: <ChatRoom />
+        }
+      ]
+    }
+  ],
+});
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
