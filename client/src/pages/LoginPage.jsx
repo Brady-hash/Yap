fd1007ceb9d7757d965da3a48974f6b812f8bdef
Brady-hash <bradydavis@Bradys-MacBook-Pro.local>
2024-04-04 15:00:09 -0500

    added signup and login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm';
+import { useNavigate } from 'react-router-dom';
+
 
 const LoginPage =  () => {
+    const navigate = useNavigate();
+
+    const navigateToSignup = () => {
+        navigate('/signup');
+    }
+
     return (
         <div>
             <h1>Login</h1>
             <LoginForm />
+            <button onClick ={navigateToSignup}> Signup </button>
         </div>
     );
 };
