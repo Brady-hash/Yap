41eb5f61edbcfebdfcb812629a98eed3cbb5f821
Psiko23 <tylerpittman2001@gmail.com>
2024-04-16 21:48:12 -0700

    fixing logo not rendering

--- client/src/components/btns/LogoBtn.jsx
+++ client/src/components/btns/LogoBtn.jsx
@@ -1,9 +1,10 @@
 import { Avatar } from "@mui/material";
+import logoSrc from '../../images/Yap-Logo.png'
 
 export const LogoBtn = () => {
     return (
         <Avatar 
-        src="/public/Yap-Logo.png"
+        src={logoSrc}
         sx={{
                 height: '100px',
                 width: '100px'
